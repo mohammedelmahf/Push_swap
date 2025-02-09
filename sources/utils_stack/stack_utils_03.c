5c517ff744de3ee0bc1b07bc34cbefdfb36deba0
Mahammed Elmahfoudi <mohammedelmahfoudi2004@gmail.com>
2025-02-09 16:06:05 +0100

    125%

--- sources/utils_stack/stack_utils_03.c
+++ sources/utils_stack/stack_utils_03.c
@@ -6,34 +6,11 @@
 /*   By: maelmahf <maelmahf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/17 09:09:13 by maelmahf          #+#    #+#             */
-/*   Updated: 2025/02/08 19:07:46 by maelmahf         ###   ########.fr       */
+/*   Updated: 2025/02/09 14:14:19 by maelmahf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
-
-// long long	longlong_atoi(const char *s)
-// {
-// 	long long	reslt;
-// 	int			sign;
-
-// 	reslt = 0;
-// 	sign = 1;
-// 	while (*s && ((*s >= 9 && *s <= 13) || *s == ' '))
-// 		s++;
-// 	if (*s == '-' || *s == '+')
-// 	{
-// 		if (*s == '-')
-// 			sign = -1;
-// 		s++;
-// 	}
-// 	while (*s && (*s >= '0' && *s <= '9'))
-// 	{
-// 		reslt = reslt * 10 + (*s - '0');
-// 		s++;
-// 	}
-// 	return (sign * reslt);
-// }
 
 long long	longlong_atoi(const char *s)
 {
